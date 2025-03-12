595819cf919e39c5fbc3c72ab036b3e38c984775
Amandine <amandine.moigno@gmail.com>
2025-03-12 16:58:53 -0400

    added font-bold attribute for selected language

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,23 +1,29 @@
 import { useTranslation } from "react-i18next"
+import { useState } from "react"
 
 export default function LanguageSwitcher() {
   const { i18n } = useTranslation()
-
+  const [selectedLang, setSelectedLang] = useState<string>("fr")
   const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang)
+    setSelectedLang(lang)
   }
 
   return (
     <div className="font-quicksand">
       <button
-        className="cursor-pointer hover:underline"
+        className={`cursor-pointer hover:underline ${
+          selectedLang === "en" ? "font-bold" : ""
+        }`}
         onClick={() => changeLanguage("en")}
       >
         EN
       </button>
       <span>{" | "}</span>
       <button
-        className=" cursor-pointer hover:underline"
+        className={`cursor-pointer hover:underline ${
+          selectedLang === "fr" ? "font-bold" : ""
+        }`}
         onClick={() => changeLanguage("fr")}
       >
         FR
